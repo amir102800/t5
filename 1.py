fb8e0cc0face25f285e7ba98203d863778dc62c2
Mohammad-Mahdi-kaseb <mahdi15.20781338@gmail.com>
2020-07-22 17:33:23 +0430

    csv log

--- 1.py
+++ 1.py
@@ -6,25 +6,46 @@ def turn():
     global n
     n += 1
     numbers.append(n + 1)
-    print(numbers)
     d = datetime.now()
     a3.config(text=d.strftime('%c'))
-    a2.config(text=numbers[-1])
+    e1.config(text='your turn:' + str(numbers[-1]))
+    e2.config(text='remain:' + str(len(numbers)-1))
+    line = '{},{},{}\n'.format(numbers[-1],
+                               d.strftime('%-H:%-M:%-S'),
+                               d.strftime('%-d%-m%Y'))
+    with open('listman.csv','a') as file:
+        file.write(line)
 
+
+def append_csv(number , operator):
+    d = datetime.now()
+    line = '{},{},{},operator{}\n'.format(number,
+                                          d.strftime('%-H:%-M:%-S'),
+                                          d.strftime('%-d%-m%Y'),
+                                          operator)
+
+    with open('operaotrs.csv','a') as file:
+        file.write(line)
 
 def op1():
     if numbers:
-        lo1.config(text=numbers.pop(0))
+        number = numbers.pop(0)
+        lo1.config(text=number)
+        append_csv(number,1)
 
 
 def op2():
     if numbers:
-        lo2.config(text=numbers.pop(1))
+        number=numbers.pop(0)
+        lo2.config(text=number)
+        append_csv(number , 2)
 
 
 def op3():
     if numbers:
-        lo3.config(text=numbers.pop(2))
+        number=numbers.pop(0)
+        lo3.config(text=number)
+        append_csv(number , 3)
 
 
 root = tk.Tk()
@@ -37,22 +58,33 @@ a3.grid(row=0, column=0)
 b1 = tk.Button(root, text='get turn !', command=turn)
 b1.grid(row=1, column=0)
 
-a2 = tk.Label(root, text='')
-a2.grid(row=2, column=0)
+
+e1 = tk.Label(root, text='')
+e1.grid(row=2, column=0)
+
+e2 = tk.Label(root , text= ' ')
+e2.grid(row = 4, column= 0 )
 
 tp = tk.Toplevel(root)
+
+
 o1 = tk.Button(tp, text='operators 1 ', command=op1)
 o1.grid(row=0, column=0)
+
 o2 = tk.Button(tp, text='operators 2 ', command=op2)
 o2.grid(row=0, column=1)
+
 o3 = tk.Button(tp, text='operators 3  ', command=op3)
 o3.grid(row=0, column=2)
 
 lo1 = tk.Label(tp, text='--')
 lo1.grid(row=1, column=0)
+
 lo2 = tk.Label(tp, text='--')
 lo2.grid(row=1, column=1)
+
 lo3 = tk.Label(tp, text='--')
 lo3.grid(row=1, column=2)
+
 
 root.mainloop()
---
+++ 3.py
@@ -0,0 +1,10 @@
+with open('list man.csv','a') as file:
+    file.write('mahdi\n')
+    file.write('mobin\n')
+    file.write('ghscb\n')
+    file.write('amir\n')
+    file.write('azhsdar\n')
+
+
+
+
