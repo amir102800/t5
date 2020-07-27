5aef994cb86eac284a4226ff32098417f5421265
amir102800 <amirmohammadalizadeh1111@gmail.com>
2020-07-27 17:30:15 +0430

    chest timer start

---
+++ 4.py
@@ -0,0 +1,58 @@
+import tkinter as tk
+
+
+def stop():
+    pass
+
+
+root = tk.Tk()
+
+tk.Label(root,
+         text='left player',
+         font=("times", 20, "italic")) \
+    .grid(row=0, column=0)
+tk.Label(root,
+         text='right player',
+         font=("times", 20, "italic")) \
+    .grid(row=0, column=1)
+
+
+
+l_timer = tk.StringVar()
+l_timer.set('20:00')
+tk.Label(root,
+         textvariable=l_timer,
+         font=("times", 20, "italic")) \
+    .grid(row=1, column=0)
+
+
+r_timer = tk.StringVar()
+r_timer.set('20:00')
+tk.Label(root,
+         textvariable=r_timer,
+         font=("times", 20, "italic")) \
+    .grid(row=1, column=1)
+tk.Button(root,
+          text="stop",
+          command=stop,
+          font=('courier', 20)) \
+    .grid(row=2, column=0)
+
+tk.Button(root
+          , text="stop"
+          , command=stop
+          , font=("courier", 20)) \
+    .grid(row=2, column=1)
+tk.Button(root,
+          text="start",
+          command=stop,
+          font=('courier', 30)) \
+    .grid(row=3, column=0, columnspan=2)
+
+tk.Button(root
+          , text="cancel"
+          , command=stop
+          , font=("courier", 30)) \
+    .grid(row=4, column=0, columnspan=2)
+
+root.mainloop()
